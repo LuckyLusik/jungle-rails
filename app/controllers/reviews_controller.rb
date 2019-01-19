dfa9a2adaaf06b9002984fc1c6975aaab94b0c3f
Lucy <lucyneatdesign@yahoo.ca>
2019-01-19 00:05:34 +0000

    Added DELETE button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 
+before_filter :authorize
+
   def create
     product = Product.find(params[:product_id])
     @review = product.reviews.build(review_params)
@@ -16,6 +18,12 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:product_id, :rating, :description)
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    Review.find(params[:id]).destroy
+    redirect_to product_path(params[:product_id])
   end
 
 end
